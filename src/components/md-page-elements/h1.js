3ab08bcb7fad2d3685f7fffd7bbe5b75edd0b2af
spences10 <spences10apps@gmail.com>
2020-08-31 12:23:21 +0100

    remove src to get MDX working

--- src/components/md-page-elements/h1.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components'
-
-export const H1 = styled.h1`
-  font-family: ${({ theme }) => theme.fontFamily.serif};
-  font-size: ${({ theme }) => theme.fontSize['4xl']};
-`
